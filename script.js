06ba4c78a109eb3ea1848a021be81cf0ef29bea2
maleficscorpio <70068895+maleficscorpio@users.noreply.github.com>
2022-01-19 21:00:33 +0530

    all pages complete

--- script.js
+++ script.js
@@ -17,3 +17,81 @@ for(i =0; i < len; i++){
 }
 
 
+
+
+
+
+  function hidemovies() { //series button
+    var movies = document.querySelectorAll("#movie");
+    var toggle = document.getElementsByClassName(toggle)
+    var i;
+    for (i = 0; i < movies.length; i++){
+    if (movies[i].style.display !== "none") {
+      movies[i].style.display = "none";
+      showseries();//show series
+    } else {
+      movies[i].style.display = " block";
+    }
+}
+  }
+
+function showseries(){
+    var series = document.querySelectorAll("#serie");
+    var i;
+    for (i = 0; i < series.length; i++){
+        series[i].style.display = " block";
+    }
+}
+
+
+  function hideseries() { //Movies button
+    var series = document.querySelectorAll("#serie");
+    var i;
+    for (i = 0; i < series.length; i++){
+    if (series[i].style.display !== "none") {
+      series[i].style.display = "none";
+      showmovies()//show movies
+    } else {
+      series[i].style.display = " block";
+    }
+}
+  }
+
+  function showmovies(){
+    var movies = document.querySelectorAll("#movie");
+    var i;
+    for (i = 0; i < movies.length; i++){
+        movies[i].style.display = " block";
+    }
+}
+
+
+
+// var countMovie = 1;
+// function setMovieColor(btn) {
+//     hideseries();
+//     var property = document.getElementById(btn);
+//     if (countMovie == 0) {
+//         property.style.backgroundColor = "#000000"
+//         countMovie = 1;        
+//     }
+//     else {
+//         property.style.backgroundColor = "#7FFF00"
+//         countMovie = 0;
+//     }
+// }
+
+// var countSerie = 1;
+// function setSerieColor(btn) {
+//     hidemovies();
+//     var property = document.getElementById(btn);
+//     if (countSerie == 0) {
+//         property.style.backgroundColor = "#000000"
+//         countSerie = 1;        
+//     }
+//     else {
+//         property.style.backgroundColor = "#7FFF00"
+//         countSerie = 0;
+        
+//     }
+// }
